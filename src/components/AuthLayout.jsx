1855edd4f39ff7eee6e40960a300fb1669712633
Hseyaj <codex.hseyaj26@gmail.com>
2025-03-08 23:29:49 +0530

    add authentication components: Protect, Login, Signup; implement PostCard and Select components for user interaction

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+function Protect({children, authentication=true}) {
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        if(authentication && authentication !== authStatus){
+            navigate("/login")
+        }
+        else if(!authentication && authentication!==authStatus){
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+  return (
+    loader? <h1>Loading...</h1> : null
+  )
+}
+
+export default Protect
